5833f6b56e6acb3eb3d697158b7bb5021f2c9f5b
matias-maldonado <mmaldonadoaraya@udesa.edu.ar>
2020-06-09 19:57:29 -0300

    cambios js

--- HTML/JS/playlist.js
+++ HTML/JS/playlist.js
@@ -1,24 +1,40 @@
-recuperoStorage = localStorage.getItem('playlist');
-let playlist = JSON.parse(recuperoStorage);
-let playlistWrapper = document.querySelector('.playlist');
-console.log(playlist);
+let queryString = location.search;
+let datos = new URLSearchParams (queryString);
+let idTrack= datos.get('id');
 
-function buscarTracks(idTrack) {
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let url = proxy + "https://api.deezer.com/tracks/" + idTrack
+
+let recuperoStorage = localStorage.getItem('.playlist');
+if (recuperoStorage = null){
+    playlist = [];
+}
+else{
+    playlist = JSON.parse(recuperoStorage);
+}
+let playlist = document.querySelector('.playlistClass');
+
+playlistClass.forEach(function(idTrack){
+    mostrarTrack(idTrack);
+});
+function mostrarTrack(idTrack){
     let proxy = 'https://cors-anywhere.herokuapp.com/';
-    let url = proxy + "https://api.deezer.com/track/" + idTrack
-fetch("url")
+    let url = proxy + "https://api.deezer.com/tracks/" + idTrack;
+
+    fetch("url")
     .then(function(response){
             return response.json();
         } )
 
-    .then(function (info) { 
-            console.log(info);
-            let info =document.querySelector('.primerCancion');
-            primerCancion.innerHtml += '<li>' + info.tittle + '</li>';        
+    .then(function (datos) { 
+            console.log(datos);
+            let playlistClass =document.querySelectorAll('.playlistClass');
+            let resultados = datos.data;
+            resultados.forEach(function(datos) {
+                playlistClass.innerHtml += '<li>' + datos.tittle + '</li>';    
+            });     
         })
     .catch(function(error){
         console.log(error);
     })
 }
-
-        
