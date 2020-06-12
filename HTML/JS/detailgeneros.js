8b788648ed92b31c2b33124861b66d77fd9a84d0
Gonzalo Ocampos <gonzaloocampos@MacBook-de-Gonzalo.local>
2020-06-12 19:25:26 -0300

    detailgenero

---
+++ HTML/JS/detailgeneros.js
@@ -0,0 +1,23 @@
+let proxy = 'https://cors-anywhere.herokuapp.com/';
+let url = proxy + "https://api.deezer.com/genre/idgenero/artists"
+
+let detalleGenero = document.querySelector(".detalleGenero")
+
+fetch(url)
+    .then(function(response){
+            return response.json();
+        })
+
+    .then(function (dgenero) { 
+        console.log(dgenero);
+        let xyz = dgenero.data; 
+        for(let i=0; i<10; i++) {
+            detalleGenero.innnerHTML += '<li>'+ xyz[i].type +  + xyz[i].id + xyz[i].name + '</li>' ;
+        }    
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+
+
+
