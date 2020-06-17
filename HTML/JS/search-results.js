ede06ff1beeaffe91d72b0633c40dacc8f55ebd6
milagrosechague4 <mechague@udesa.edu.ar>
2020-06-17 14:01:30 -0300

    agrego lo de ver mas resultados en search

--- HTML/JS/search-results.js
+++ HTML/JS/search-results.js
@@ -11,66 +11,120 @@ const spinner = document.getElementById("spinner");
 
 window.addEventListener('load', function() {
 
-     showSpinner()
+    showSpinner()
 
-      let queryString = new URLSearchParams(location.search)
-      let loBuscado = queryString.get("search");
+    let queryString = new URLSearchParams(location.search)
+    let loBuscado = queryString.get("search");
 
-      fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + loBuscado)
-      .then(
+    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + loBuscado)
+    .then(
         function(respuesta) {
-            return respuesta.json();            
-        }
-       )
-       .then(
-        function(info) {
-             console.log(info)
-
-             let TracksArray = info.data
-             for (let i=0; i<TracksArray.length; i++){
-                 let trackImg = TracksArray[i].artist.picture
-                 let trackTitle = TracksArray[i].title
-                 let trackArtist = TracksArray[i].artist.name
-                 let trackAlbum = TracksArray[i].album.title
-                 let trackDur = TracksArray[i].duration
-                 let img = '<img class="track-img" src="' + trackImg + '" alt="track-image"></img>'
-
-                 let idTrack = TracksArray[i].id
-                 let idAlbum = TracksArray[i].album.id
-                 let idArtista = TracksArray[i].artist.id
-
-                  let minutos = trackDur/60
-                  minutos = Math.floor(minutos)
-                  let seconds = trackDur %60
-                  if(seconds<10){ seconds = '0' + seconds }
-
-                 document.querySelector('.track-container').innerHTML += 
-                 `<article class="track-info"><div class="img-container">` + img + `</div>
-                 <a href="track.html?id=`+ idTrack + `" class="title"><p>` + trackTitle + `</p></a>
-                 <a href="detail-artista.html?id=` + idArtista + `" class="artista"><p>` + trackArtist + `</p></a>
-                 <a href="detail-album.html?id=` + idAlbum + `" class="album"><p>` + trackAlbum + `</p></a>
-                 <p class="duracion">` + minutos + `:` + seconds + `</p></article>`  
-
-                 let track = document.querySelectorAll('.track-info');
-
-                 track.forEach (function (item){
-                    item.addEventListener('mouseover', function(){
-                        this.style.backgroundColor = 'black';
-
-                    })
-                  
-                    item.addEventListener('mouseout', function(){
-                        this.style.backgroundColor= 'rgba(230, 148, 40, 0.000)';
-                    })
-                  })
-
-            
-             }
-
-        })
-        .catch(function(error){
-            console.log(error);
-        })
+        return respuesta.json();            
     })
+    .then(function(info) {
+        console.log(info)
+
+        let TracksArray = info.data
+        for (let i=0; i<TracksArray.length; i++){
+            let trackImg = TracksArray[i].artist.picture
+            let trackTitle = TracksArray[i].title
+            let trackArtist = TracksArray[i].artist.name
+            let trackAlbum = TracksArray[i].album.title
+            let trackDur = TracksArray[i].duration
+            let img = '<img class="track-img" src="' + trackImg + '" alt="track-image"></img>'
+
+            let idTrack = TracksArray[i].id
+            let idAlbum = TracksArray[i].album.id
+            let idArtista = TracksArray[i].artist.id
+
+            let minutos = trackDur/60
+            minutos = Math.floor(minutos)
+            let seconds = trackDur %60
+            if(seconds<10){ seconds = '0' + seconds }
+
+            document.querySelector('.track-container').innerHTML += 
+            `<article class="track-info"><div class="img-container">` + img + `</div>
+            <a href="track.html?id=`+ idTrack + `" class="title"><p>` + trackTitle + `</p></a>
+            <a href="detail-artista.html?id=` + idArtista + `" class="artista"><p>` + trackArtist + `</p></a>
+            <a href="detail-album.html?id=` + idAlbum + `" class="album"><p>` + trackAlbum + `</p></a>
+            <p class="duracion">` + minutos + `:` + seconds + `</p></article>`  
+
+            let track = document.querySelectorAll('.track-info');
+
+            track.forEach (function (item){
+                item.addEventListener('mouseover', function(){
+                    this.style.backgroundColor = 'black';
+                })
+                  
+                item.addEventListener('mouseout', function(){
+                    this.style.backgroundColor= 'rgba(230, 148, 40, 0.000)';
+                })
+            })
+            
+        }
+      
+        let next = info.hasOwnProperty('next');
+        let linkNext = info.next;
+        
+        if (next == true) {
+            document.querySelector('.boton').innerHTML += '<a class="more" href="">VER MAS RESULTADOS</a>';
+    
+            document.querySelector('.more').addEventListener('click', function(e) {
+                e.preventDefault();
+    
+                fetch('https://cors-anywhere.herokuapp.com/' + linkNext)
+                .then(function(respuesta) {
+                    return respuesta.json();            
+                })
+                .then(function(datos) {
+                    next = datos.hasOwnProperty('next');
+                    linkNext = datos.next;
+    
+                    if (next == true) {
+                        let TracksArray = datos.data
+                                
+                        for (let i=0; i<TracksArray.length; i++){
+                            let trackImg = TracksArray[i].artist.picture
+                            let trackTitle = TracksArray[i].title
+                            let trackArtist = TracksArray[i].artist.name
+                            let trackAlbum = TracksArray[i].album.title
+                            let trackDur = TracksArray[i].duration
+                            let img = '<img class="track-img" src="' + trackImg + '" alt="track-image"></img>'
+
+                            let idTrack = TracksArray[i].id
+                            let idAlbum = TracksArray[i].album.id
+                            let idArtista = TracksArray[i].artist.id
+
+                            let minutos = trackDur/60
+                            minutos = Math.floor(minutos)
+                            let seconds = trackDur %60
+                            if(seconds<10){ seconds = '0' + seconds }
+
+                            document.querySelector('.track-container').innerHTML += 
+                            `<article class="track-info"><div class="img-container">` + img + `</div>
+                            <a href="track.html?id=`+ idTrack + `" class="title"><p>` + trackTitle + `</p></a>
+                            <a href="detail-artista.html?id=` + idArtista + `" class="artista"><p>` + trackArtist + `</p></a>
+                            <a href="detail-album.html?id=` + idAlbum + `" class="album"><p>` + trackAlbum + `</p></a>
+                            <p class="duracion">` + minutos + `:` + seconds + `</p></article>`  
+
+                            let track = document.querySelectorAll('.track-info');
+
+                            track.forEach (function (item){
+                                item.addEventListener('mouseover', function(){
+                                     this.style.backgroundColor = 'black';
+                                })
+                                item.addEventListener('mouseout', function(){
+                                     this.style.backgroundColor= 'rgba(230, 148, 40, 0.000)';
+                                })
+                            })
+                        }
+                    } else {
+                        document.querySelector('.botones').innerHTML = '';
+                    }
+                }) 
+            })           
+        }
+    })       
+})
 
 
